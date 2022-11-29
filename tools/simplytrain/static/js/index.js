90063aad1b67e8fd5de52a500d98900629225e1a
nilusink <nilusink@protonmail.com>
2022-11-29 14:42:06 +0100

    Base web app
    Took 59 minutes

---
+++ tools/simplytrain/static/js/index.js
@@ -0,0 +1,9 @@
+/*
+index.js
+29. November 2022
+
+Functions for index.html
+
+Author:
+Nilusink
+*/
--- tools/simplytrain/wsgi.py
+++ tools/simplytrain/wsgi.py
@@ -9,19 +9,23 @@ Loads translations from CSV and asks user random translation
 questions. Translation direction can be set.
 """
 
-from flask import Flask, request
+from flask import Flask, request, render_template, jsonify
 import setloader as sl
 import json
 
+
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def root():
+    return render_template("index.html")
+
 
 @app.route("/sets")
 def get_sets():
-    return sl.get_available_sets()
+    return jsonify(sl.get_available_sets())
+
 
 @app.route("/compose")
 def get_compose():
@@ -35,4 +39,4 @@ def get_compose():
     nr_alts = int(alts_string)
     sets = sets_string.split(",")
     wordlist = sl.compose_exercise(sets, nr_words, nr_alts)
-    return wordlist
+    return jsonify(wordlist)
