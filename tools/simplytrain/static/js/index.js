b8f614966fc966129e2e224af538d7b0364a135a
melektron <matteo@elektron.work>
2022-11-29 15:17:29 +0100

    added set selection getter

--- tools/simplytrain/static/js/index.js
+++ tools/simplytrain/static/js/index.js
@@ -7,6 +7,23 @@ Functions for index.html
 Author:
 Nilusink
 */
+
+function getSetSelection() {
+    let selected_sets = []
+    let checks = document.getElementById("setsBox").children;
+    
+    for (var i = 0; i < checks.length; i++) {
+        let checkbox = checks[i].getElementsByTagName("input")[0];
+        let checklabel = checks[i].getElementsByTagName("label")[0];
+        if (checkbox.checked) {
+            selected_sets.push(checklabel.innerText)
+        }
+    }
+    console.log(selected_sets)
+    return selected_sets
+}
+
+
 function getWords() {
     fetch("/compose?nr=500&alts=0&sets=BS_100-101,BS_102-105")
         .then((response) => {
