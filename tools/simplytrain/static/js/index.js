37c37ce4a849322593ee824747811159d2fb3abd
nilusink <nilusink@protonmail.com>
2022-11-29 15:08:04 +0100

    15:07
    Took 26 minutes

--- tools/simplytrain/static/js/index.js
+++ tools/simplytrain/static/js/index.js
@@ -7,3 +7,12 @@ Functions for index.html
 Author:
 Nilusink
 */
+function getWords() {
+    fetch("/compose?nr=500&alts=0&sets=BS_100-101,BS_102-105")
+        .then((response) => {
+            return response.json()
+        })
+        .then({
+
+        })
+}
