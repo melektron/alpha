e60f1c03816dd3929a312f7263f7b14f2027a03d
MrFlunter <flunterpertinger@tsn.at>
2024-03-01 14:07:09 +0100

    added structure

--- server/main.py
+++ server/main.py
@@ -2,6 +2,18 @@ import json
 import asyncio
 from websockets.server import serve 
 
+#Example for the questions in json 
+"""
+data = [
+    {"question": "In welche klasse gehe ich?", "type": 0, "answer": ["4AHEL", "4BHEL", "4AHBT", "4AHET"]}
+]
+"""
+
+
+def get_questions(): 
+    data = json.load(open("questions.json", "r"))
+    message = json.dumps(data[0])
+
 async def echo(websocket):
     async for message in websocket:
         await websocket.send(message)
@@ -10,13 +22,5 @@ async def main():
     async with serve(echo, "localhost", 8765):
         await asyncio.Future()  # run forever
 
-asyncio.run(main())
-
-
-data = json.load(open("questions.json", "r"))
-
-data = [
-    {"question": "In welche klasse gehe ich?", "type": 0, "answer": ["4AHEL", "4BHEL", "4AHBT", "4AHET"]}
-]
-
-message = json.dumps(data[0])
+if __name__ == "__main__": 
+    asyncio.run(main())
