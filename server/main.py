235d3963bff031f676a4b4d906a575156b9572fc
MrFlunter <flunterpertinger@tsn.at>
2024-03-08 13:59:47 +0100

    added sorted questions

--- server/main.py
+++ server/main.py
@@ -1,6 +1,8 @@
 import json
 import asyncio
 from websockets.server import serve 
+import random
+import websockets
 
 #Example for the questions in json 
 """
@@ -10,17 +12,23 @@ data = [
 """
 
 
-def get_questions(): 
+def get_questions(num_questions)->list: 
     data = json.load(open("questions.json", "r"))
-    message = json.dumps(data[0])
+    chosen_questions = random.sample(data, num_questions)
+    return chosen_questions
 
-async def echo(websocket):
+        
+
+async def echo(websocket:websockets.WebSocketServerProtocol):
     async for message in websocket:
         await websocket.send(message)
 
 async def main():
+    x=get_questions(2) 
+    print(x)
+    """
     async with serve(echo, "localhost", 8765):
         await asyncio.Future()  # run forever
-
+    """
 if __name__ == "__main__": 
     asyncio.run(main())
