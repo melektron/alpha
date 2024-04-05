4843ae060561f4a67789678faab71e4faa20938b
MrFlunter <flunterpertinger@tsn.at>
2024-04-05 14:03:50 +0200

    added decoding of ranking message

--- client/main.py
+++ client/main.py
@@ -14,6 +14,7 @@ def main():
     print(response)  # Acknowledge name received
 
     while True:
+        ranking = client.recv(1024).decode()
         question = client.recv(1024).decode()
 
         print(question)
