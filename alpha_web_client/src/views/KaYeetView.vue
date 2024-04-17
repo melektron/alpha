5f2361b1772cf4240550da6260f43ba6c70dcf63
melektron <matteo@elektron.work>
2024-04-17 23:31:28 +0200

    added some ui for kayeet connect

--- alpha_web_client/src/views/KaYeetView.vue
+++ alpha_web_client/src/views/KaYeetView.vue
@@ -9,12 +9,22 @@ Home page with overview
 
 <script setup lang="ts">
 
+function connectToGame(payload: Event) {
+    console.log("Connecting to:", payload)
+}
+
 </script>
 
 
 <template>
     <main>
-        KaYeet?!
+        <div class="connect-wrapper">
+            <h1> KaYeet?! KaYeet!! </h1>
+            <form @submit.prevent="connectToGame" class="connect-form">
+                <InputText placeholder="Game Host Address" />
+                <Button type="submit">Join Game</Button>
+            </form>
+        </div>
     </main>
 </template>
 
@@ -22,10 +32,21 @@ Home page with overview
 <style scoped>
 
 main {
+    display: grid;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    background-color: #381272;
+}
+
+.connect-form {
     display: flex;
     flex-direction: column;
-    flex-wrap: nowrap;
     gap: 10px;
+    padding: 20px;
+    background-color: var(--surface-400);
+    border-radius: 10px;
 }
 
 </style>
